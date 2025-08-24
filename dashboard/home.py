a8a6d74544bc603bf8206c4085490d5519711176
deepanshi jaiswal <dineshkumar983852@gmail.com>
2025-08-24 16:00:18 +0530

    change

--- dashboard/home.py
+++ dashboard/home.py
@@ -3,4 +3,4 @@ st.title("welcome to the titanic dashboard")
 
 st.markdown('<h4 style="color: red; border : 1px solid black">this dashboard gives a complete'
 'and visuals available in the application.</h4>',unsafe_allow_html = True)
-st.image("")
+st.image("assets/titanic.jpg", caption='Titanic Dataset Overview', use_column_width=True)
